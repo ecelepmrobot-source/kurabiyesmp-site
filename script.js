2cd98515c964c545cd26bd9fc0113e9011363839
ecelepmrobot-source <e.celep_mrobot@icloud.com>
2026-08-28 22:52:49 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const SERVER_IP="stank-usher.tun.ply.gg";
-function copyIp(){
-  navigator.clipboard.writeText(SERVER_IP);
-  const t=document.getElementById("toast");
-  t.classList.add("show");
-  setTimeout(()=>t.classList.remove("show"),1600);
-}
